e8892b9a586ecc1b1badf39ae51490f84d70a151
theFox6 <the.fox6@gmx.de>
2020-02-14 11:16:55 +0100

    add documentation to vp_util

--- vp_util.lua
+++ vp_util.lua
@@ -1,7 +1,19 @@
+---
+--A module with lots of helpers for the VenusParser.
+--
+--@module vp_util
 local vp_util = {}
 
---TODO: documentation
-
+---
+--Find the first match of a set of patterns within a string.
+--
+--The pattern, that can be found at the earliest position within the string is used to match.
+--If two patterns are at the same position the shorter match is returned.
+--
+--@function [parent=#vp_util] find_min_match
+--@param #string str the string to be searched for the patterns
+--@param patterns the pattern or patterns that are searched within the string
+--@return the starting and the end position of the match
 function vp_util.find_min_match(str,patterns)
   local pats
   local patt = type(patterns)
@@ -34,6 +46,19 @@ function vp_util.find_min_match(str,patterns)
   return minspos, matchepos
 end
 
+---
+--A generator to iterate over a string splitting it wherever matches of a pattern can be found.
+--
+--A single or multiple patterns are searched within a string.
+--If the string starts with a matching sequence the sequence itself is given to the iteration.
+--If a matching sequence is found further within the string the sequence before the match is given to the iteration.
+--Every sequence given to the iteration is removed from the iterator.
+--Like this it will traverse the string splitting it into the matches and non-matches.
+--
+--@function [parent=#vp_util] optmatch
+--@param #string str the string to search for matches
+--@param patterns the pattern or patterns to split by
+--@return #function the iterator for a loop
 function vp_util.optmatch(str,patterns)
   local cutstr = str
   return function()
@@ -62,15 +87,30 @@ function vp_util.optmatch(str,patterns)
   end
 end
 
-function vp_util.gen1_table(gen,...)
+---
+--A function generating a table from an iterator.
+--
+--@function [parent=#vp_util] gen_table
+--@param #function it the iterator to use in a for loop 
+--@return #table the table containing the returns of the iterator
+function vp_util.gen_table(it)
   local tab = {}
-  for el in gen(...) do
+  for el in it do
     table.insert(tab,el)
   end
   return tab
 end
 
--- double_flat_table_compare
+---
+--double flat table compare  
+--A function comparing the contents of two tables.
+--
+--It iterates over both tables checking if the other contains the same elements.
+--
+--@function [parent=#vp_util] dftc
+--@param #table t1 the table to compare with t2
+--@param #table t2 the table to compare with t1
+--@return #boolean whether the tables contents are the same
 function vp_util.dftc(t1,t2)
   for i,el in pairs(t1) do
     if t2[i] ~= el then
@@ -85,6 +125,8 @@ function vp_util.dftc(t1,t2)
   return true
 end
 
+---
+--The unit tests for the vp utilities.
 local function tests()
   assert(vp_util.dftc({},{}))
   assert(vp_util.dftc({1},{1}))
@@ -92,10 +134,10 @@ local function tests()
   assert(vp_util.dftc({1,"2",true},{1,"2",true}))
   assert(not vp_util.dftc({true,"1",1},{1,1,1}))
   
-  assert(vp_util.dftc(vp_util.gen1_table(vp_util.optmatch,"123","123"),{"123"}))
-  assert(vp_util.dftc(vp_util.gen1_table(vp_util.optmatch,"123","321"),{"123"}))
-  assert(vp_util.dftc(vp_util.gen1_table(vp_util.optmatch,"123", "1"), {"1","23"}))
-  assert(vp_util.dftc(vp_util.gen1_table(vp_util.optmatch,"123", "2"), {"1","2","3"}))
+  assert(vp_util.dftc(vp_util.gen_table(vp_util.optmatch("123","123")),{"123"}))
+  assert(vp_util.dftc(vp_util.gen_table(vp_util.optmatch("123","321")),{"123"}))
+  assert(vp_util.dftc(vp_util.gen_table(vp_util.optmatch("123", "1")), {"1","23"}))
+  assert(vp_util.dftc(vp_util.gen_table(vp_util.optmatch("123", "2")), {"1","2","3"}))
 end
 
 tests()
